0999f2ff44986bfac789a946cae6d5083f538dc4
Lider <liderolia@inbox.ru>
2023-02-14 20:35:41 +0700

    homework_06

---
+++ homework_06/Notebook.java
@@ -0,0 +1,79 @@
+package homework_06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notebook {
+    private String id;
+    private int RAM;
+    private String OS;
+    private int HDD;
+    private String model;
+
+    public Notebook(String id, int RAM, String OS, int HDD, String model) {
+        this.id = id;
+        this.RAM = RAM;
+        this.OS = OS;
+        this.HDD = HDD;
+        this.model = model;
+    }
+
+    public static List<String> filterProperties() {
+        List<String> list = new ArrayList<>();
+        list.add("RAM");
+        list.add("OS");
+        list.add("HDD");
+        list.add("model");
+
+        return list;
+    }
+
+    @Override
+    public String toString() {
+        return "Ноутбук ID " + id + ": " +
+                "Модель " + model +
+                ", RAM " + RAM +
+                ", Система " + OS +
+                ", SSD " + HDD;                
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String name) {
+        this.id = name;
+    }
+
+    public int getRam() {
+        return RAM;
+    }
+
+    public void setRam(int RAM) {
+        this.RAM = RAM;
+    }
+
+    public String getOperatingSystem() {
+        return OS;
+    }
+
+    public void setOperatingSystem(String OS) {
+        this.OS = OS;
+    }
+
+    public int getHard() {
+        return HDD;
+    }
+
+    public void setHard(int hdd) {
+        this.HDD = hdd;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
